1a93c6ede91db28c1417b33fe4c07946523fd07a
Jessica Froese <jessicafroese@hotmail.ca>
2021-04-09 16:10:44 -0500

    Added RegexApp and ToWords

---
+++ RegexApp/RegexApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexApp 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The default regular expression checks for "
+            + "at least one letter.");
+
+            while (true)
+            {
+                Console.Write("\nEnter a regular expression"
+                + "(or press ENTER to use the default): ");
+
+                ConsoleKeyInfo cki = Console.ReadKey();
+                string pattern = string.Empty;
+                string userInput = string.Empty;
+
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    pattern = @"^[a-z]+$";
+                    Console.Write("\n^[a-z]+$");
+                }
+
+                if (string.IsNullOrEmpty(pattern))
+                {
+                    pattern = Console.ReadLine();
+                    pattern = cki.KeyChar + pattern;
+                }
+
+                Console.WriteLine("\nEnter some input: ");
+
+                userInput = Console.ReadLine();
+
+                PrintIsMatch(userInput, pattern);
+
+                Console.WriteLine("Press ESC to end or any key to try again.");
+
+                cki = Console.ReadKey();
+
+                if (cki.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("H" + "Have a nice day!");
+        }
+
+        public static void PrintIsMatch(string value, string pattern)
+        {
+            Regex re = new Regex($"{pattern}", RegexOptions.IgnoreCase);
+
+            Console.WriteLine($"{value} and {pattern}: {re.IsMatch(value)}");
+        }
+    }
+}
---
+++ ToWords/ToWords/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Text.Json;
+
+namespace CSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "185050000";
+            BigInteger bi = BigInteger.Parse(userInput);
+
+            Console.WriteLine(ToWords(bi));
+        }
+
+
+        public static string ToWords(BigInteger number)
+        {
+            string[] partsByThree = number.ToString("N0").Split(",");
+            string res = string.Empty;
+
+            for (int i = 0; i < partsByThree.Length; i++)
+            {
+                if (partsByThree[i] != "000")
+                {
+                    res += !string.IsNullOrEmpty(res) ? " " : "";
+                    res += PartToWords(partsByThree[i]);
+                    res += " " + LargeNumberToWord(partsByThree.Length - i - 1);
+                }
+            }
+
+            return res;
+        }
+
+
+
+        public static string LargeNumberToWord(int value)
+        {
+            string[] largeMap = new[]
+                { "", "thousand", "million", "billion", "trillion",
+                "quadrillion", "quintillion", "sextillion", "septillion",
+                "octillion", "nonillion", "decillion" };
+
+            return largeMap[value];
+        }
+
+        public static string PartToWords(string part)
+        {
+            part = part.PadLeft(3, '0');
+
+            string[] unitsMap = new[]
+            { "", "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine", "ten", "eleven",
+                "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+                "seventeen", "eighteen", "nineteen" };
+
+            string[] tensMap = new[]
+            {"", "ten", "twenty", "thirty", "forty", "fifty",
+                "sixty", "seventy", "eighty", "ninety" };
+
+            int index;
+            string result = string.Empty;
+
+            // transform hundreds
+            if (part[0] != '0') 
+            {
+                index = Convert.ToInt32(part[0].ToString());
+                result += unitsMap[index] + " hundred";
+            }
+
+            // transform teens
+            string tens = string.Empty;
+            if (part[1] == '1')
+            {
+                index = Convert.ToInt32(part[1].ToString() + part[2].ToString());
+                tens = unitsMap[index];
+
+                result += string.IsNullOrEmpty(result) ? "" : " and ";
+                result += tens;
+
+                return result;
+            }
+            else if (part[1] != '0')
+            {
+                index = Convert.ToInt32(part[1].ToString());
+                tens = tensMap[index];
+            }
+
+            string oneDigit = string.Empty;
+
+            if (part[2] != '0')
+            {
+                index = Convert.ToInt32(part[2].ToString());
+                oneDigit = unitsMap[index];
+            }
+
+            result += !string.IsNullOrEmpty(result)
+                      && !string.IsNullOrEmpty(tens) ? " and " : "";
+            result += tens;
+
+            result += !string.IsNullOrEmpty(result)
+                        && string.IsNullOrEmpty(tens)
+                        && !string.IsNullOrEmpty(oneDigit) ? " and " : "";
+
+            result += !string.IsNullOrEmpty(tens)
+                       && !string.IsNullOrEmpty(oneDigit) ? "-" : "";
+            result += oneDigit;
+
+            return result;
+        }
+    }
+}
